0d90465fe6b1635a4b5e87c4e9eb6c7a2205f6b7
Nikola Jokic <jokicnikola07@gmail.com>
2023-01-18 21:56:38 +0100

    created proxy env variables helper consulting the secret ref

--- controllers/actions.github.com/clientutil.go
+++ controllers/actions.github.com/clientutil.go
@@ -2,7 +2,11 @@ package actionsgithubcom
 
 import (
 	"context"
+	"fmt"
+	"net/url"
 
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/types"
 	kclient "sigs.k8s.io/controller-runtime/pkg/client"
 )
 
@@ -19,4 +23,12 @@ func patch[T object[T]](ctx context.Context, client patcher, obj T, update func(
 	original := obj.DeepCopy()
 	update(obj)
 	return client.Patch(ctx, obj, kclient.MergeFrom(original))
+}
+
+func getProxyUserInfoBySecretNamespacedName(ctx context.Context, client kclient.Reader, namespacedName types.NamespacedName) (*url.Userinfo, error) {
+	secret := new(corev1.Secret)
+	if err := client.Get(ctx, namespacedName, secret); err != nil {
+		return nil, fmt.Errorf("failed to get http proxy credential secret: %w", err)
+	}
+	return url.UserPassword(string(secret.Data["username"]), string(secret.Data["password"])), nil
 }
